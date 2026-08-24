578eee52761ba82ee0abe73405a11405aba48f56
maheen.raza <maheen.raza@ucalgary.ca>
2026-08-24 14:57:48 -0600

    adding 3 new columns

--- transformation.py
+++ transformation.py
@@ -5,6 +5,15 @@ df = pd.read_excel(excel_data)
 
 df.rename(columns={'incident_info': 'Location', 'description': 'Description', 'start_dt': 'Recorded', 'modified_dt': 'Last Updated', 'quadrant': 'Quadrant', 'longitude': 'Longitude', 'latitude': 'Latitude', 'count': 'Incident Count', 'id': 'Incident ID', 'point': 'Point'}, inplace=True)  # renaming the columns for clarity
 
-df_reduced = df.iloc[:, :-3]   # dropping the last three columns since they're not needed
-df_reduced.to_excel("CalgaryTrafficIncidentsData.xlsx", index=False)
+df["Recorded"] = pd.to_datetime(df["Recorded"])  # converting the 'Recorded' column to datetime format
+
+df["Year"] = df["Recorded"].dt.year  # extracting the year from the 'Recorded' column
+df["Month"] = df["Recorded"].dt.strftime('%B')  # extracting the month from the 'Recorded' column
+df["Day"] = df["Recorded"].dt.day  # extracting the day from the 'Recorded' column
+
+columns_to_drop = [":@computed_region_kxmf_bzkv", ":@computed_region_4a3i_ccfj", ":@computed_region_4b54_tmc4"]
+
+df.drop(columns=columns_to_drop, inplace=True)
+
+df.to_excel(excel_data, index=False)  # saving the transformed data to a new Excel file
 
